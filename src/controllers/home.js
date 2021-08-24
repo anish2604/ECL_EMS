5b3f3bdad57eb230a7cd55e8c3000f6eb777b37c
Anish Chattaraj <82085341+anish2604@users.noreply.github.com>
2021-08-24 14:58:57 +0530

    Delete src directory

--- src/controllers/home.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-const home = (req, res) => {
-  return res.sendFile(path.join(__dirname, "./templates/views/profile.ejs"));
-};
-
-module.exports = {
-  getHome: home
-};
