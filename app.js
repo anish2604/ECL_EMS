28aa4d8d397130ba7eff66fe855efeab9f31f2f0
Anish Chattaraj <anishfiitjee1719@gmail.com>
2021-08-25 15:48:42 +0530

    Add gitignore, procfile and updated ports

--- app.js
+++ app.js
@@ -628,3 +628,5 @@ app.post("/contactUS_index", async(req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// if(process.env.NODE_ENV === 'production') {}
